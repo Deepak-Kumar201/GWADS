54fef973d246d6a0ad2ad119dc11bcaeaa128829
Deepak-Kumar201 <dk10112002@gmail.com>
2022-02-17 10:13:46 +0530

    login added

--- Server/routes/Images.js
+++ Server/routes/Images.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const Image = require("../models/Image");
+const User = require("../models/User");
 
 router.post("/upload", async (req, resp) => {
     try {
         var data = {
-            URL : req.body.URL
-        }
+            URL: req.body.URL,
+        };
         var img = new Image(data);
         await img.save();
-        resp.send({"Success":"Images Uploaded"})
+        resp.send({ Success: "Images Uploaded" });
     } catch (error) {
         resp.status(500).send({ error: "Please try again later!" });
     }
 });
 
-module.exports = router
+router.post("/generateRandom", async (req, resp) => {
+    try {
+        var user = await User.findOne({ username: req.body.username });
+        if(!user){
+            resp.status(400).send({error:"No user found"});
+            return;
+        }
+        var data1 = await Image.find({ _id: { $in: user.authimages } });
+        var data2 = await Image.find({ _id: { $nin: user.authimages } }).limit(
+            10 - user.authimages.length
+        );
+        var data1 = data1.map((elem) => {
+            return elem.URL;
+        });
+        var data2 = data2.map((elem) => {
+            return elem.URL;
+        });
+        data2 = data2.concat(data1);
+        resp.send({ ImageData: data2, success: "Image Fetched" });
+    } catch (error) {
+        resp.status(500).send({ error: "Please try again later!" });
+    }
+});
+
+module.exports = router;
---
+++ auth/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { useState } from "react";
+import "./style.css";
+import img1 from "../Image/1-min.jpg"
+import img2 from "../Image/2-min.jpg"
+import img3 from "../Image/3-min.jpg"
+import img4 from "../Image/4-min.jpg"
+import img5 from "../Image/5-min.jpg"
+import img6 from "../Image/6-min.jpg"
+import img7 from "../Image/8-min.jpg"
+
+export default function Homepage() {
+    const [images, setImages] = useState([img1,img2,img3,img4,img5,img6,img7]);
+    const nextLogin = async (e)=>{
+        console.log("object");
+        e.preventDefault();
+        const username = document.getElementById("username");
+        if(username.value.trim().length == 0){
+            window.alert("Enter A valid username");
+            return;
+        }
+        var data = await fetch("http://localhost:5000/api/image/generateRandom",{
+            method:'POST',
+            body:JSON.stringify({username : username.value}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        data = await data.json();
+
+        if(data.error){
+            window.alert("Enter A valid Username");
+            return;
+        }else{
+            document.querySelector(".formLogin").style.left = "-100vw";
+            document.querySelector(".loginimageSelector").style.left = "0px";
+            // setImages(data.ImageData);
+        }
+    }
+
+    
+    const previousstep = ()=>{
+        document.querySelector(".formLogin").style.left = "0vw";
+        document.querySelector(".loginimageSelector").style.left = "100vw";
+
+    }
+
+    const loginMe = async ()=>{
+        const elem = document.getElementsByClassName("logincheck");
+        const username = document.getElementById("username");
+        var checked = [];
+        for(var i of elem){
+            if(i.checked){
+                checked.push(i.getAttribute("url"));
+            }
+        }
+        console.log(checked);
+        window.localStorage.setItem("loginUser",username.value);
+        window.localStorage.setItem("loginURL", checked);
+        const bodydata = {
+            "username":username.value,
+            "imagesURL":checked
+        }
+
+        var data = await fetch("http://localhost:5000/api/user/login",{
+            method:'POST',
+            body:JSON.stringify(bodydata),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        data = await data.json();
+        console.log(data);
+        if(data.error){
+            window.alert("Selected Images are not correct");
+        }else{
+            window.alert("Login Successful");
+        }
+
+        window.localStorage.removeItem("loginUser");
+        window.localStorage.removeItem("loginURL");
+
+    }
+
+    return (
+        <div className="container">
+            <div className="formLogin">
+                <form className="loginform">
+                    <input
+                        type="text"
+                        id="username"
+                        placeholder="Username"
+                    />
+                    <input type="submit" value="Next" id="login" onClick={nextLogin}/>
+                    <a href="#" className="createNew">
+                        Create New Account
+                    </a>
+                </form>
+            </div>
+            <div className="loginimageSelector">
+                <div className="imageCont">
+                    {
+                        images.map((elem, index)=>{
+                            return(
+                            <>
+                                <input type="checkbox" id={"login"+index} style={{display:"none"}} url={elem} onChange={(e)=>{
+                                    if(e.target.checked){
+                                        document.getElementById("image"+index).classList.add("bordered")
+                                    }else{
+                                        document.getElementById("image"+index).classList.remove("bordered");
+                                    }
+                                }} className="logincheck"/>
+                                <label htmlFor={"login" + index}> <img src={elem} width="150px" id={"image"+index}/></label>
+                            </>
+                            )
+                        })
+                    }
+                </div>
+                <div className="buttons">
+                    <button id="loginBack" onClick={previousstep}>Back</button>
+                    <button id="loginMe" onClick={loginMe}>Login</button>
+                </div>
+            </div>
+        </div>
+    );
+}
